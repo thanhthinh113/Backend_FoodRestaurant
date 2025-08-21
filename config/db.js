ce846a7c924a8e21f7d6377547548a278ba8b0a0
thanhthinh113 <146800825+thanhthinh113@users.noreply.github.com>
2025-08-21 11:49:21 +0700

    sửa lại file env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-    )
-    .then(
-      () => {
-        console.log("MongoDB connected successfully");
-      },
-      (err) => {
-        console.error("MongoDB connection error:", err);
-      }
-    );
+  await mongoose.connect(process.env.MONGO_DB).then(
+    () => {
+      console.log("MongoDB connected successfully");
+    },
+    (err) => {
+      console.error("MongoDB connection error:", err);
+    }
+  );
 };
