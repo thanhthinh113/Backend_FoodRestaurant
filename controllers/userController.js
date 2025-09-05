5efd6fb94b651b3d93a3969965a5b513e54cea51
thanhthinh113 <146800825+thanhthinh113@users.noreply.github.com>
2025-09-05 11:08:10 +0700

    feat(user): cập nhật thời gian token

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ const loginUser = async (req, res) => {
 };
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET);
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 //register user
